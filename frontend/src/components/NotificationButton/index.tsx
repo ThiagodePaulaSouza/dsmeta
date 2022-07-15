8d4029e33ea8e72356686e3b7d2a23978a539583
Thiago de Paula Souza <thiagodepaulasouza@gmail.com>
2022-07-15 20:38:17 -0300

    send notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,9 +1,21 @@
+import axios from "axios";
 import Icon from "../../assets/img/notification-icon.svg";
+import { BASE_URL } from "../../utils/request";
 import "./styles.css";
 
-export function NotificationButton() {
+interface Props {
+  saleId: number;
+}
+
+const handleClick = (id: number) => {
+  axios
+      .get(`${BASE_URL}/sales/${id}/notification`)
+      .then((response) => alert("Notificação enviada com sucesso!"))
+}
+
+export function NotificationButton({ saleId }: Props) {
   return (
-    <div className="dsmeta-red-btn">
+    <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
       <img src={Icon} alt="Notificar" />
     </div>
   );
